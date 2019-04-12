c56e0802041f401e9b9a34d0c7ea4fe052d4bdd4
jennampe <jenna.perry@miami.edu>
2019-04-12 14:34:08 -0400

    large update
    correct image scrolling opacity changes, add more images, add more content, add resume PDF

--- index.js
+++ index.js
@@ -1,73 +1,96 @@
-var allImages = $(".animate-image");
+$(".content-section").each(function() {
+	$(this).css("padding-top", $(".navbar").outerHeight()+"px");
+});
 
-var viewHeight = $(window).height() - $(".navbar").outerHeight(); // viewable area; browser height, minus navbar height
+var allImages = $(".animate-image");
+allImages.css("opacity", 0);
+
+var magicAmt = 0.1; // what percent of view height "counts" as exactly at top of window? in viewheight percents, what is size of "magic window"?
+
+var viewHeight;
+var bufferSize;
+function initializeViewVars()
+{
+	viewHeight = $(window).height() - $(".navbar").outerHeight(); // viewable area; browser height, minus navbar height
+	bufferSize = (1-magicAmt) * viewHeight; // size of "buffer zone" where an image is partially visible
+};
+initializeViewVars();
+
 
 var contentSection = allImages.parent();
-var divStart = contentSection.offset().top + $(".navbar").outerHeight(); // point in scroll at which "true" top of div starts
+var divStart = contentSection.offset().top + $(".navbar").outerHeight() - (contentSection.outerHeight() - contentSection.height());  // scroll point at which "true" top of div is located
 var divStop = divStart + contentSection.height();
-
-alert(divStart);
-
 var viewStart = divStart - viewHeight; // first scroll position at which any point of the div is visible
-var viewStop = divStop + viewHeight; // last scroll position at which any point of the div is visible
+var viewStop = divStop - $(".navbar").outerHeight();; // last scroll position at which any point of the div is visible
 
+/*$(window).scroll(function() {
+	scrollDist = $(window).scrollTop();
+	
+	curImage = allImages;
+	if (Math.abs(scrollDist - divStart) <= (magicAmt*viewHeight)) // div is almost exactly at top of window
+	{
+		curImage.css("opacity", 1);
+	} else if (scrollDist < viewStart | scrollDist > viewStop) // div is invisible
+	{
+		curImage.css("opacity", 0);
+	} else // div is partly visible
+	{		
+		if (scrollDist >= viewStart & scrollDist <= (viewStart + bufferSize)) // scroll position is above div start
+		{
+			curImage.css("opacity", (scrollDist - viewStart) / bufferSize);
+		}
+		else if (scrollDist >= (viewStop - bufferSize) & scrollDist <= viewStop) // scroll position is below div start
+		{
+			curImage.css("opacity", (viewStop - scrollDist) / bufferSize);
+		}
+	}
+});*/
+
+function checkImageOpacity(curImage) // update image opacity
+{
+	var contentSection = curImage.parent();
+	var divStart = contentSection.offset().top + $(".navbar").outerHeight() - (contentSection.outerHeight() - contentSection.height());  // scroll point at which "true" top of div is located
+	var divStop = divStart + contentSection.height();
+	var viewStart = divStart - viewHeight; // first scroll position at which any point of the div is visible
+	var viewStop = divStop - $(".navbar").outerHeight();; // last scroll position at which any point of the div is visible
+	
+	if (Math.abs(scrollDist - divStart) <= (magicAmt*viewHeight)) // div is almost exactly at top of window
+	{
+		curImage.css("opacity", 1);
+	} else if (scrollDist < viewStart | scrollDist > viewStop) // div is invisible
+	{
+		curImage.css("opacity", 0);
+	} else // div is partly visible
+	{		
+		if (scrollDist >= viewStart & scrollDist <= (viewStart + bufferSize)) // scroll position is above div start
+		{
+			curImage.css("opacity", (scrollDist - viewStart) / bufferSize);
+		}
+		else if (scrollDist >= (viewStop - bufferSize) & scrollDist <= viewStop) // scroll position is below div start
+		{
+			curImage.css("opacity", (viewStop - scrollDist) / bufferSize);
+		}
+	}
+	
+	if(viewStop > scrollDist & scrollDist + $(window).height() == $(document).height())
+	{
+		curImage.fadeTo("fast", 1);
+		//curImage.css("opacity", 1);
+	}
+	
+	return;
+}
+
+// run for the first time
+var scrollDist = 0;
+allImages.each(function(){
+		checkImageOpacity($(this));
+});
+	
 $(window).scroll(function() {
-	if ($(window).scrollTop() < viewStart | $(window).scrollTop() > viewStop) // if offscreen, then invisible
-	{
-		allImages.css("opacity", 0);
-	} else if ($(window).scrollTop() == viewStart) // div is exactly at top of window
-	{
-		allImages.css("opacity", 1);
-	} else if ($(window).scrollTop() > viewStart & $(window).scrollTop() < viewStop) // top part of div is visible, bottom part of div is not
-	{
-		var percentThru = ($(window).scrollTop() - viewStart) / (viewStop - viewStart);
-		allImages.css("opacity", percentThru);
-	}
-	
-	/*var percentThruDiv = ($(window).scrollTop() - topLevel) / (bottomLevel - topLevel);
-	
-	if (percentThruDiv > 1 | percentThruDiv < 0) // if offscreen, invisible
-	{
-		allImages.css("opacity", 0);
-	} else if (percentThruDiv > 0.4 & percentThruDiv < 0.6) // if in middle, 100% opacity
-	{
-		allImages.css("opacity", 100);
-	} else if (percentThruDiv <= 0.4)
-	{
-		allImages.css("opacity", (percentThruDiv / 0.4));
-	} else if (percentThruDiv >= 0.6)
-	{
-		allImages.css("opacity", (((0.6 - percentThruDiv) / 0.4) + 1));
-	}
-	*/
+	scrollDist = $(window).scrollTop();
+	allImages.each(function(){
+		checkImageOpacity($(this));
+	});
 });
 
-allImages.click(function() {
-	alert($(window).scrollTop());
-});
-
-/*$(window).scroll(function() {
-	alert($(window).scrollTop());
-});*/
-
-
-/*$(window).scroll(function() {
-	alert("A wild scroll appears!");
-});*/
-
-
-/*allImages.each(function(){
-	/*
-	$(this).bind("click", function(){
-		alert($(this).attr("src"));
-	});	*/
-	/*
-	var contentSection = $(this).parent();
-	
-	var topLevel = contentSection.offset().top;
-	var bottomLevel = topLevel + contentSection.height();
-	
-	alert(topLevel);
-	alert(bottomLevel);
-	*/
-//});
